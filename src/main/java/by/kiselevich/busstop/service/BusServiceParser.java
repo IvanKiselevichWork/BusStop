86dc7255b54045a5582d8caae7ff60700b23a03e
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-20 11:46:34 +0300

    Add parseInputData to BusServiceParser

--- src/main/java/by/kiselevich/busstop/service/BusServiceParser.java
+++ src/main/java/by/kiselevich/busstop/service/BusServiceParser.java
@@ -3,8 +3,12 @@ package by.kiselevich.busstop.service;
 import by.kiselevich.busstop.domain.BusService;
 import by.kiselevich.busstop.domain.ServiceName;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BusServiceParser {
 
@@ -12,10 +16,10 @@ public class BusServiceParser {
     private static final int SERVICE_NAME_INDEX = 0;
     private static final int DEPARTURE_TIME_INDEX = 1;
     private static final int ARRIVAL_TIME_INDEX = 2;
-//
-//    public List<BusService> parseInputData(String inputFilename) {
-//        return bull;
-//    }
+
+    public List<BusService> parseInputData(String inputFilename) throws IOException {
+        return Files.readAllLines(Path.of(inputFilename)).stream().map(this::parseLine).collect(Collectors.toList());
+    }
 
     public BusService parseLine(String string) {
         String[] strings = string.split(RAW_DATA_SPLIT_REGEX);
