df577a76400516fb5c330236e31f224748bf72e3
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-20 11:42:59 +0300

    Add equals and hashcode to BusService

--- src/main/java/by/kiselevich/busstop/domain/BusService.java
+++ src/main/java/by/kiselevich/busstop/domain/BusService.java
@@ -1,12 +1,14 @@
 package by.kiselevich.busstop.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalTime;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class BusService {
     private final ServiceName serviceName;
     private final LocalTime departureTime;
