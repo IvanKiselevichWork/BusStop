a8a8fc9ee13a86bfc00591363b4ad59562a69e39
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-20 10:46:41 +0300

    Add filename checking to BusStopApplication

--- src/main/java/by/kiselevich/busstop/BusStopApplication.java
+++ src/main/java/by/kiselevich/busstop/BusStopApplication.java
@@ -1,7 +1,21 @@
 package by.kiselevich.busstop;
 
+import by.kiselevich.busstop.exception.NoFilenameException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class BusStopApplication {
+
+    private static final Logger LOG = LogManager.getLogger(BusStopApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOG.info("Get args count: {}", args.length);
+        if (args.length > 0) {
+            LOG.info("Get filename: {}", args[0]);
+            System.out.println("Hello World!");
+        } else {
+            LOG.error("No input filename");
+            throw new NoFilenameException("No input filename!");
+        }
     }
 }
