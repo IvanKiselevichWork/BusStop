24a65d293eb9a845bfa94971d9129053afca6724
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-20 11:58:54 +0300

    Add tests for parseInputData method

--- src/test/java/by/kiselevich/busstop/service/BusStopParserTest.java
+++ src/test/java/by/kiselevich/busstop/service/BusStopParserTest.java
@@ -2,14 +2,35 @@ package by.kiselevich.busstop.service;
 
 import by.kiselevich.busstop.domain.BusService;
 import by.kiselevich.busstop.domain.ServiceName;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class BusStopParserTest {
 
-    private BusServiceParser busServiceParser = new BusServiceParser();
+    private static final String TEST_FILENAME = "TEST_FILE.txt";
+
+    private final BusServiceParser busServiceParser = new BusServiceParser();
+
+    @BeforeClass
+    public static void init() throws IOException {
+        Files.write(Path.of(TEST_FILENAME), "Posh 10:10 11:00\nGrotty 10:10 11:00".getBytes());
+    }
+
+    @AfterClass
+    public static void deInit() throws IOException {
+        Files.delete(Path.of(TEST_FILENAME));
+    }
 
     @Test
     public void test1() {
@@ -28,5 +49,28 @@ public class BusStopParserTest {
     @Test()
     public void test3() {
         Assert.assertThrows(ArrayIndexOutOfBoundsException.class, () -> busServiceParser.parseLine("Posh 10:15"));
+    }
+
+    @Test
+    public void test4() throws IOException {
+        List<BusService> actualBusServiceList = busServiceParser.parseInputData(TEST_FILENAME);
+        List<BusService> expectedBusServiceList = List.of(
+                new BusService(ServiceName.POSH, LocalTime.of(10, 10), LocalTime.of(11, 0)),
+                new BusService(ServiceName.GROTTY, LocalTime.of(10, 10), LocalTime.of(11, 0)));
+        Assert.assertArrayEquals(actualBusServiceList.toArray(), expectedBusServiceList.toArray());
+    }
+
+    @Test
+    public void test5() throws IOException {
+        List<BusService> actualBusServiceList = busServiceParser.parseInputData(TEST_FILENAME);
+        List<BusService> expectedBusServiceList = List.of(
+                new BusService(ServiceName.POSH, LocalTime.of(10, 10), LocalTime.of(11, 0)),
+                new BusService(ServiceName.GROTTY, LocalTime.of(10, 10), LocalTime.of(11, 1)));
+        Assert.assertFalse(Arrays.equals(actualBusServiceList.toArray(), expectedBusServiceList.toArray()));
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertThrows(NoSuchFileException.class, () -> busServiceParser.parseInputData(TEST_FILENAME + "123"));
     }
 }
