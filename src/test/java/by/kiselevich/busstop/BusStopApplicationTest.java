e722ff6392a534ebd6dab986ac9ce3115305d233
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-20 14:11:16 +0300

    Add tests for application

--- src/test/java/by/kiselevich/busstop/BusStopApplicationTest.java
+++ src/test/java/by/kiselevich/busstop/BusStopApplicationTest.java
@@ -20,4 +20,49 @@ public class BusStopApplicationTest {
         List<String> actualData = Files.readAllLines(Path.of("output.txt"));
         Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
     }
+
+    @Test
+    public void test2() throws IOException, URISyntaxException {
+        String fullFilename = Paths.get(getClass().getClassLoader().getResource("test_data2.txt").toURI()).toString();
+        BusStopApplication.main(new String[]{fullFilename});
+        List<String> expectedData = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("output2.txt").toURI()));
+        List<String> actualData = Files.readAllLines(Path.of("output.txt"));
+        Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
+    }
+
+    @Test
+    public void test3() throws IOException, URISyntaxException {
+        String fullFilename = Paths.get(getClass().getClassLoader().getResource("test_data3.txt").toURI()).toString();
+        BusStopApplication.main(new String[]{fullFilename});
+        List<String> expectedData = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("output3.txt").toURI()));
+        List<String> actualData = Files.readAllLines(Path.of("output.txt"));
+        Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
+    }
+
+    @Test
+    public void test4() throws IOException, URISyntaxException {
+        String fullFilename = Paths.get(getClass().getClassLoader().getResource("test_data4.txt").toURI()).toString();
+        BusStopApplication.main(new String[]{fullFilename});
+        List<String> expectedData = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("output4.txt").toURI()));
+        List<String> actualData = Files.readAllLines(Path.of("output.txt"));
+        Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
+    }
+
+    @Test
+    public void test5() throws IOException, URISyntaxException {
+        String fullFilename = Paths.get(getClass().getClassLoader().getResource("test_data5.txt").toURI()).toString();
+        BusStopApplication.main(new String[]{fullFilename});
+        List<String> expectedData = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("output5.txt").toURI()));
+        List<String> actualData = Files.readAllLines(Path.of("output.txt"));
+        Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
+    }
+
+    @Test
+    public void test6() throws IOException, URISyntaxException {
+        String fullFilename = Paths.get(getClass().getClassLoader().getResource("test_data6.txt").toURI()).toString();
+        BusStopApplication.main(new String[]{fullFilename});
+        List<String> expectedData = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource("output6.txt").toURI()));
+        List<String> actualData = Files.readAllLines(Path.of("output.txt"));
+        Assert.assertArrayEquals(expectedData.toArray(), actualData.toArray());
+    }
 }
